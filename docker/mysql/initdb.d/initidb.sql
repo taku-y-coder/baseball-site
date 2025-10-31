4329acd8ca7cc7ef3d80f01aacfbdfdfa646bdf7
taku-y-coder <tak_yamamoto.js1@outlook.jp>
2025-10-31 22:55:14 +0900

    ローカルの修正を反映

---
+++ docker/mysql/initdb.d/initidb.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS pet
+(
+    name VARCHAR(20),
+    owner VARCHAR(20),
+    species VARCHAR(20),
+    sex CHAR(1),
+    birth DATE,
+    death DATE
+);
+
+INSERT INTO pet
+VALUES ('Puffball', 'Diane', 'hamster', 'f', '1999-03-30', NULL);
